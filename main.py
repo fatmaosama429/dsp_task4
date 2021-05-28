204d22195287a10dc1af375e4408eac281004113
fatmaosama429 <fatma.abdelrahim99@eng-st.cu.edu.eg>
2021-05-28 20:51:22 +0200

    add

--- main.py
+++ main.py
@@ -35,63 +35,114 @@ class MainApp(QtWidgets.QMainWindow,From_Main):
         super(MainApp, self).__init__()
         self.setupUi(self)
         music= pd.read_excel('/home/fatma/dsp_task4/songsDataBase6.xlsx')
+        self.songnames=music["song"].tolist()
         self.DataBase_songs = music[:].values  
-        print(len(self.DataBase_songs))
         logger.info("init")
-
         logger.info("songs data base has been saved and it has "+str(len(self.DataBase_songs))+" songs in it")
-        print("songs data base has been saved and it has "+str(len(self.DataBase_songs))+" songs in it")
         self.definefunctions()
         
         # variables 
-        self.hashes=[]
-        
+        self.mixersongs=[None,None]
+        self.songs_frate=[None,None]
+        self.MIX_RATIO.hide()
+        self.RATIO.hide()
+        self.resultsTable.hide()
+           
     def definefunctions(self):
         self.actionLoad_song.triggered.connect(lambda:self.compare_1song())
-
-    def compare_1song(self):
+        self.SONG1.clicked.connect(lambda:self.compare_2songs(0))
+        self.SONG2.clicked.connect(lambda:self.compare_2songs(1))
+        self.MIX_RATIO.sliderReleased.connect(lambda:self.get_mixer_similarity())
+        
+    def read(self):
         load_file = QtWidgets.QFileDialog.getOpenFileName(None, "Load Audio File %s",filter="*.mp3")
         path=load_file[0]
         audiofile = AudioSegment.from_mp3(path)[:60000] 
-        self.data = np.array(audiofile.get_array_of_samples())
-        self.rate = audiofile.frame_rate
-        print(self.data)
+        data = np.array(audiofile.get_array_of_samples())
+        rate = audiofile.frame_rate
+        # print(self.data)
         logger.info("the song in uploaded with data")
-        logger.info(str(self.data))
-        self.get_hashes_list()    
-        self.get_similarity_indexes()
-        
-    def get_hashes_list(self):
-            sampleFreqs,sampleTime, colorMesh = signal.spectrogram(self.data,fs=self.rate)
-            self.spect_hash=self.get_hash(colorMesh)
-            self.hashes.append(self.spect_hash)
-            for fet in self.get_features(self.data,colorMesh,self.rate):
-                self.hashes.append(self.get_hash(fet))
+        logger.info(str(data))
+        # print("data=",data,"rate",rate)
+        return data,rate
+    
+    def compare_1song(self):
+        logger.info("comparing 1 song with the database")
+        data,rate=self.read()
+        hashes_list =self.get_hashes_list(data,rate)    
+        similarityindex=self.get_similarity_indexes(hashes_list)
+
+    def mixer(self):
+        logger.info("mixing 2 songs' data")
+        self.slidervalue=self.MIX_RATIO.value() / 100.0
+        if (len(self.mixersongs[0])<= len(self.mixersongs[1])):
+            self.new_data=self.mixersongs[0]*self.slidervalue+(1-self.slidervalue)*self.mixersongs[1][:len(self.mixersongs[0])]
+            return self.new_data,self.songs_frate[0]
+        else:
+            self.new_data=self.mixersongs[0][:len(self.mixersongs[1])]*self.slidervalue+(1-self.slidervalue)*self.mixersongs[1]
+            return self.new_data,self.songs_frate[1]
+    
+    def compare_2songs(self,id):
+        logger.info("adding song of id "+str(id)+"to the compare function")
+        self.mixersongs[id],self.songs_frate[id]=self.read()
+       
+        if(id==1):
+            self.MIX_RATIO.setValue(0)
+            self.MIX_RATIO.show()
+            self.RATIO.show()
+            logger.info("2 songs were added to the mixer")
+            self.get_mixer_similarity()
+        else: print("there is 1 song only ")
+           
+    def get_mixer_similarity(self):
+        data,rate=self.mixer()
+        logger.info("extracting the similarity indexes")
+        hashes=self.get_hashes_list(data,rate)
+        similarity=self.get_similarity_indexes(hashes)
+        self.get_table(similarity)
+        print("similarity indexes ",self.all_similarity_index)
+                  
+    def get_hashes_list(self,data,rate):
+            self.hashes=[]
+            sampleFreqs,sampleTime, colorMesh = signal.spectrogram(data,fs=rate)
+            spect_hash=self.get_hash(colorMesh)
+            self.hashes.append(spect_hash)
+            for feature in self.get_features(data,colorMesh,rate):
+                self.hashes.append(self.get_hash(feature))
             logger.info("the song hashes=")
             logger.info(self.hashes)
-            print(self.hashes)
+            return self.hashes
     
     def get_hash(self,array):
         dataInstance = Image.fromarray(array)
-        P_HASH= imagehash.phash(dataInstance, hash_size=16).__str__()
-        print(P_HASH)
-        return P_HASH
+        PHASH= imagehash.phash(dataInstance, hash_size=16).__str__()
+        return PHASH
 
     def get_features(self,data,color,rate):
         features_list=[lib.feature.mfcc(y=data.astype('float64'),sr=rate),
                lib.feature.melspectrogram(y=data,sr=rate,S=color),
                lib.feature.chroma_stft(y=data,sr=rate,S=color)]
-        print(features_list)
         return features_list
 
-    def get_similarity_indexes(self):
+    def get_similarity_indexes(self,hashes):
         logger.info("get similarity index")
-        self.all_similarity_index = similarity_indexes(self.DataBase_songs,self.hashes)
+        self.all_similarity_index = similarity_indexes(self.DataBase_songs,hashes)
+        return self.all_similarity_index
         
+    def get_table(self,similarity_list):
+        # self.resultsTable.show()
+        self.resultsTable.setColumnCount(2)
+        self.resultsTable.setRowCount(len(similarity_list))
+        for row in range(len(similarity_list)):
+            self.resultsTable.setItem(row, 1, QtWidgets.QTableWidgetItem( self.songnames[row] )  )
+            self.resultsTable.setItem(row, 0, QtWidgets.QTableWidgetItem(str(round(similarity_list[row], 2))+"%"))
+     
+            self.resultsTable.verticalHeader().setSectionResizeMode(row, QtWidgets.QHeaderView.Stretch)
 
-        print(self.all_similarity_index)
-        
-        
+        self.resultsTable.setHorizontalHeaderLabels([ "similarity percentage","song names"])
+        self.resultsTable.show()
+
+
     
       
 
--- similarityfile.py
+++ similarityfile.py
@@ -13,15 +13,6 @@ logger.setLevel(logging.DEBUG)
 
 logger.info("file similarity.py attached succesfully")
 
-spect_hashes_differences  = []       
-feature1_hashes_difference = []       
-feature2_hashes_differences = []       
-feature3_hashes_differences = []       
-hash_differences_list =[spect_hashes_differences,feature1_hashes_difference,\
-    feature2_hashes_differences,feature3_hashes_differences]
-Similarity_indexes_list  = []        
-
-
        
 def getHammingDistance(hash1: str, hash2: str, index : int) -> int:
     try:
@@ -33,6 +24,15 @@ def getHammingDistance(hash1: str, hash2: str, index : int) -> int:
 
   
 def similarity_indexes(list_of_songs: list,list_of_song_hashes: list):
+
+    spect_hashes_differences   = []       
+    feature1_hashes_difference = []       
+    feature2_hashes_differences = []       
+    feature3_hashes_differences = []       
+    hash_differences_list =[spect_hashes_differences,feature1_hashes_difference,\
+        feature2_hashes_differences,feature3_hashes_differences]
+    Similarity_indexes_list  = []        
+
     logger.info("similarity_indexes function is called")
     for i in range(len(list_of_songs)): #compare between my song and all songs in my data base 
         
@@ -55,36 +55,3 @@ def similarity_indexes(list_of_songs: list,list_of_song_hashes: list):
     logger.info("similarity function done successfully")
     return Similarity_indexes_list
  
-# from math import ceil
-# from imagehash import hex_to_hash
-
-# hammingDifferences  = []       # List contains the hamming distance of spectrogram hashes of all the songs and the mix
-# feature1Differences = []       # List contains the hamming distance of feature 1 hashes of all the songs and the mix
-# feature2Differences = []       # List contains the hamming distance of feature 2 hashes of all the songs and the mix
-# feature3Differences = []       # List contains the hamming distance of feature 3 hashes of all the songs and the mix
-# differences_list =[hammingDifferences,feature1Differences,feature2Differences,feature3Differences]
-# avgSimilaritiesAll  = []        # List contains the similarity percentage of all the songs and the mix (Using average hash of Spectrogram and all the features)
-
-  
-# def similarity_indexes(songs: list,feature_hash: list):
-#     for i in range(len(songs)):
-#         sum=0
-#         for j in range(len(differences_list)):
-#             print(songs[i][j+1])
-#             differences_list[j].append(getHammingDistance(hash1=songs[i][j+1], hash2=feature_hash[j]))
-        
-#         for j in range(len(differences_list)):
-#             sum =sum + differences_list[j][i]
-#         avg=sum/4
-#         avgMap = mapRanges(avg, 0, 255, 0, 1)  
-#         result = (1 - avgMap) * 100
-#         avgSimilaritiesAll.append(result) # list of similarity index have the same index of he song
-#     print("averg similarity = ",avgSimilaritiesAll)
-#     return avgSimilaritiesAll
-        
-# def getHammingDistance(hash1: str, hash2: str) -> int:
-#     return hex_to_hash(hash1) - hex_to_hash(hash2)
-
-# def mapRanges(inputValue: float, inMin: float, inMax: float, outMin: float, outMax: float):
-#     slope = (outMax-outMin) / (inMax-inMin)
-#     return outMin + slope*(inputValue-inMin)
